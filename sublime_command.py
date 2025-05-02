2c8872541dbbec64cdee88b9bbe6f010615800cc
nemo6 <miguel.maudet.contact@gmail.com>
2025-05-03 00:03:16 +0200

    Update sublime_command.py

--- sublime_command.py
+++ sublime_command.py
@@ -11,13 +11,13 @@ def remove_element(target_array,list_value):
 def loop_select_word(view):
 	for p in view.sel():
 		word = view.word(p)
-		if word and (p.begin() == p.end()) :
+		if word and ( p.begin() == p.end() ) :
 			if p.contains(word):
-				return
+				continue
 			a = word.begin()
 			b = word.end()
 			view.sel().add( sublime.Region( a,b ) )
-			view.show( first_selection )
+			view.show( word )
 
 def isAfter( a, b ):
 	return a.begin() > b.begin()
@@ -27,7 +27,7 @@ def isAfter( a, b ):
 
 class select_next_variable(sublime_plugin.TextCommand):
 	def run(self,edit):
-		not_a_variable = False #
+		not_a_variable = False
 		no_selection = False
 		string_scope = False
 		view = sublime.active_window().active_view()
@@ -45,14 +45,13 @@ class select_next_variable(sublime_plugin.TextCommand):
 		# if( len(h) != 0 ):
 		# 	h.pop(0)
 		# print( current_selection["text"], first_selection )
-		# print(*h,sep="\n")
+		# print( *h,sep="\n" )
 
-		# if( len( view.sel() ) > 1 ):
-		# 	print("multi-selection")
-		# 	if "string" in view.scope_name( view.sel()[0].begin() ):
-		# 		print("in a string scope")
-		# 	else:
-		# 		print("not in a string scope")
+		if( len( view.sel() ) > 1 ):
+			# print("multi-selection")
+			if "string" in view.scope_name( view.sel()[0].begin() ):
+				# print("in a string scope")
+				not_a_variable = True
 
 		if( ( first_selection.begin() == first_selection.end() ) and len(view.sel()) == 1 ):
 			no_selection = True
@@ -76,10 +75,10 @@ class select_next_variable(sublime_plugin.TextCommand):
 			view.show( first_selection )
 			return
 
-		# print( "Is a variable", current_selection["text"], first_selection )
+		# print( *list(filter( lambda x: x == current_selection["text"], str_variables )) )
+		# print( "Is a variable" )
 		# print( *str_variables )
 		# print( *slice_selection )
-		# print( *list(filter( lambda x: x == current_selection["text"], str_variables )) )
 
 		for object_position in slice_selection:
 			text = view.substr(object_position)
@@ -100,6 +99,40 @@ class add_backtick(sublime_plugin.TextCommand):
 		b = first_selection.end()
 		self.view.insert( edit, a , "`" )
 		self.view.insert( edit, b+1 , "`" )
+
+class add_cursor_comma(sublime_plugin.TextCommand):
+	def run(self,edit):
+		view = sublime.active_window().active_view()
+		print( *view.sel() )
+		cursor_selection = view.sel()[-1]
+		cursor_pos = view.sel()[0].begin()
+		a = view.sel()[0].begin()
+		b = view.sel()[0].end()
+		next_char = view.substr( sublime.Region( cursor_pos , cursor_pos + 1 ) )
+		after_comma = { "region": sublime.Region( a + 1 , b + 2 ) , "word": view.word( sublime.Region( a + 1 , b + 2 ) )}
+		print( "next_char", next_char )
+		print( "after_comma", *list( map( lambda x : ord(x) , view.substr(after_comma["word"]) ) ) )
+
+		if( len(next_char) != 0 and next_char == "," ):
+			word = view.word(cursor_pos)
+			word_text = view.substr(word)[:-1]
+			if( word_text.isspace() ):
+				return
+			if word:
+				a = word.begin()
+				b = word.end()
+				view.sel().add( sublime.Region( a,b ) )
+				view.show( word )
+			if( view.substr(after_comma["word"])[0] != "," ):
+				a = after_comma["word"].begin()
+				b = after_comma["word"].end()
+				view.sel().add( sublime.Region( a,b ) )
+				view.show( word )
+
+			new_cursor_pos = cursor_pos + 1
+			view.sel().add(sublime.Region(new_cursor_pos))
+		else:
+			print(None)
 
 """
 
